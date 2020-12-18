6300f22477db861d988c2f86d7f7391bd4b103c4
Felipe Lopez <felipe@nogson.com>
2020-12-18 19:13:16 +0000

    El entrypoint revisa por la existencia de bd

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
-# DATABASE_FILE=`pwd`"database/fivbo.db"
+ DATABASE_FILE="fivbo.db"
 # export DATABASE_URL="sqlite:///"+$DATABASE_FILE 
 
 
-# if [ ! -f "$DATABASE_FILE"]; then
-#     flask db init
-#     flask db migrate -m "first database migrate"
-#     flask db upgrade
-# fi
-flask db init
-flask db migrate -m "first database migrate"
-flask db upgrade
+ if [ ! -f "$DATABASE_FILE" ]; then
+     flask db init
+     flask db migrate -m "first database migrate"
+     flask db upgrade
+ fi
+# flask db init
+# flask db migrate -m "first database migrate"
+# flask db upgrade
 
 gunicorn -b 0.0.0.0:8000 app:app
 
